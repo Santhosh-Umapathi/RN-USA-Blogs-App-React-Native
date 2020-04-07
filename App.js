863a509ddaaa5d073bfb1d28ba627cd554915957
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-07 15:00:21 +0200

    Context created

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+//Context
+import {BlogProvider} from './src/context/BlogContext'
+//Screens
 import HomeScreen from './src/screens/HomeScreen';
 
 const MainNavigator = createStackNavigator(
@@ -12,13 +16,22 @@ const MainNavigator = createStackNavigator(
       title: "Blogs",
       headerTintColor: "white", //Header button colors
       headerStyle: {
-        backgroundColor: "#45CE30", // header bg color
+        backgroundColor: "#F4C724", // header bg color
       },
       headerTitleStyle: {
         color: "white", // header font color
-        fontSize: 22, // header font Size
+        fontSize: 20, // header font Size
       },
     },
   }
 );
-export default createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+
+export default () =>
+{
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
